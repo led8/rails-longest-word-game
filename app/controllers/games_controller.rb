af63e9b18ab2de5f7683fe729bc01668c6c10b58
Arthur Dhuy <a.dhuy@orange.fr>
2019-01-28 16:17:53 +0100

    Fix longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,33 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
+    # @letters = Array.new(5) { VOWELS.sample }
+    @letters = VOWELS.sample(5)
+    # @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters += (('A'..'Z').to_a - VOWELS).sample(5)
+    @letters.shuffle!
   end
 
   def score
+    @letters = params[:letters].split
+    @word = params[:word].upcase
+    @included_word = included_word?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included_word?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    url = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(url.read)
+    json['found']
   end
 end
